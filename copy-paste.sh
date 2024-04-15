5688869cbe94a2abf86227545cb2c1a5a516adfb
RITHESH S <raxen001@tutamail.com>
2024-04-15 14:55:38 +0530

    added -p

--- copy-paste.sh
+++ copy-paste.sh
@@ -1,7 +1,7 @@
 wl-paste  -t TEXT > .tmp.sql
-pup -f ./.tmp.sql '.ace_content' text{} > .tmp2.sql
-cat .tmp2.sql |  sed 's/&gt\;/>/' | sed 's/&lt\;/</' > .tmp3.sql
-sqlformat .tmp3.sql --reindent --keywords upper > .tmp4.sql
+pup -f ./.tmp.sql -p '.ace_content' text{} > .tmp2.sql
+#cat .tmp2.sql |  sed 's/&gt\;/>/' | sed 's/&lt\;/</' | sed "s/&#39/\'/" > .tmp3.sql
+sqlformat .tmp2.sql --reindent --keywords upper > .tmp3.sql
 echo "Debug: 2s"
 sleep 2
-ydotool type -f .tmp4.sql
+ydotool type -f .tmp3.sql

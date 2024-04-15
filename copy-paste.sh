e6927e6e29206e974768c95c20b5cd1368d91908
RITHESH S <raxen001@tutamail.com>
2024-04-15 15:57:01 +0530

    fixed a few things

--- copy-paste.sh
+++ copy-paste.sh
@@ -2,8 +2,8 @@ wl-paste > .tmp
 pup -f ./.tmp -p '.ace_content' text{} > .tmp2.sql
 npx sql-formatter .tmp2.sql > .tmp3.sql
 #sed $'s/[^[:print:]\t]//g' .tmp3.sql > .tmp4.sql
-cat .tmp4.sql | tr '\n' '`' | sed 's/`/\ \n/g' > .tmp5.sql
+cat .tmp3.sql | tr '\n' '`' | sed 's/`/\ \n/g' > .tmp4.sql
 #cat ./.tmp5.sql
 echo "Debug: 2s"
 sleep 2
-ydotool type -d 10 -f .tmp5.sql
+ydotool type -d 10 -f .tmp4.sql
